80edbb3b1ff4ddbf028a34fb99adf18fd0d653a7
anandt1-spektra <95517953+anandt1-spektra@users.noreply.github.com>
2026-08-28 00:02:10 +0530

    Update logontask-02.ps1

--- build-frontier/logontask-02.ps1
+++ build-frontier/logontask-02.ps1
@@ -18,7 +18,7 @@ code --install-extension dbaeumer.vscode-eslint
 sleep 5
 Function CloneLabFiles
 {
-    $GitHubToken = "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"
+    $GitHubToken = "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"
 
     $RepoUrl     = "https://$GitHubToken@github.com/CloudLabsAI-Azure/MsIQ-cplt-agntsfrntr.git"
     $Branch      = "post-build-SPLabs"
